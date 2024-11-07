f8b26fecaaeeba88a3186f6819df643bd0ae72d0
Zizzazivan <zizzazivan@mail.ru>
2024-11-07 20:12:26 +0300

    цвупдшорцпршщ

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,31 @@
+package web.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.models.Car;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import web.service.CarService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class CarController {
+    private final CarService carService;
+
+    CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping(value = "/cars")
+    public String showCars(@RequestParam(name = "count", required = false) Integer count, Model model) {
+        if(count != null && count > 0 && count <= 5) {
+            model.addAttribute("cars", carService.listCars(count));
+        } else {
+            model.addAttribute("cars",carService.getAllCars());
+        }
+        return "cars";
+    }
+}
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,29 @@
+package web.service;
+
+import web.models.Car;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService {
+    List<Car> cars = new ArrayList<>();
+
+    public CarServiceImpl() {
+        cars.add(new Car(1,"ss","red"));
+        cars.add(new Car(2,"b8","blue"));
+        cars.add(new Car(3,"ree33","white"));
+        cars.add(new Car(4,"df1234","black"));
+        cars.add(new Car(5,"mariquri","orange"));
+    }
+
+    @Override
+    public List<Car> listCars(int number) {
+        return cars.subList(0, number);
+    }
+    @Override
+    public List<Car> getAllCars() {
+        return cars;
+    }
+}
