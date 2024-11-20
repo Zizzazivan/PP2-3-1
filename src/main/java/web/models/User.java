312a3f8abb6a8dd6b357d03d53395352ff0f917d
Zizzazivan <zizzazivan@mail.ru>
2024-11-20 10:57:39 +0300

    model equals & hashcode fix

--- src/main/java/web/models/User.java
+++ src/main/java/web/models/User.java
@@ -66,15 +66,12 @@ public class User {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(id, user.id) &&
-                Objects.equals(name, user.name) &&
-                Objects.equals(email, user.email) &&
-                Objects.equals(password, user.password);
+        return Objects.equals(id, user.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, email, password);
+        return Objects.hashCode(id);
     }
 
     @Override
